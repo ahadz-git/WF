3b0ad3b67c417cadc2a309350e4efdbc11933100
A H A D <93983627+ahadz-git@users.noreply.github.com>
2022-02-18 11:28:24 +0530

    Delete bot.sh

--- bot.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/ahadz-git/mirror-leech-telegram-bot.git /app; cd /app
-curl -sL https://raw.githubusercontent.com/ahadz-git/nothing/main/mirrortest/config.env -o config.env
-docker build . --rm --force-rm --compress --no-cache=true --pull --file Dockerfile -t bot
-docker run --privileged --rm -i bot
